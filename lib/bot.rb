0eb409dd548f87c1aeece32ff752f9a5727fb42f
Martin Cervantes <cervantes.martine@gmail.com>
2020-05-29 15:28:36 -0600

    Added bot general functionality

--- lib/bot.rb
+++ lib/bot.rb
@@ -13,4 +13,20 @@ class Bot
       config.access_token_secret = 'YOUR_ACCESS_SECRET'
     end
   end
+
+  def tweet(message)
+    @client.update(message)
+  end
+
+  def follow(id)
+    @client.follow(id)
+  end
+
+  def fetch_user(id)
+    @client.user(id)
+  end
+
+  def search(message)
+    @client.search(message)
+  end
 end
