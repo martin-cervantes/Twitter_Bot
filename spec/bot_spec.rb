e4b3e4d9cf89f6c1ef06e64146e2a5d02851d3da
Martin Cervantes <cervantes.martine@gmail.com>
2020-05-29 16:15:02 -0600

    Added testing method

---
+++ spec/bot_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative '../lib/bot.rb'
+
+describe Bot do
+  let (:bot) { Bot.new }
+
+  describe '#search_amlovers' do
+    it 'Check if not Integer' do
+      expect(bot.search_amlovers).to_not eql(Integer)
+    end
+
+    it 'Check if not String' do
+      expect(bot.search_amlovers).to_not eql(String)
+    end
+
+    it 'Check if return key and values' do
+      expect(bot.search_amlovers).to include(a_kind_of(Array))
+    end
+  end
+end
