17ce49203f582c88692c73fde8fa6bcfb44d43cd
Martin Cervantes <cervantes.martine@gmail.com>
2020-06-02 11:27:23 -0600

    Added global variables files

--- lib/bot.rb
+++ lib/bot.rb
@@ -2,32 +2,17 @@
 
 # frozen_string_literal: true
 
+require_relative 'pass.rb'
 require 'twitter'
 
 class Bot
   def initialize
     @client = Twitter::REST::Client.new do |config|
-      config.consumer_key = 'YOUR_CONSUMER_KEY'
-      config.consumer_secret = 'YOUR_CONSUMER_SECRET'
-      config.access_token = 'YOUR_ACCESS_TOKEN'
-      config.access_token_secret = 'YOUR_ACCESS_SECRET'
+      config.consumer_key = YOUR_CONSUMER_KEY
+      config.consumer_secret = YOUR_CONSUMER_SECRET
+      config.access_token = YOUR_ACCESS_TOKEN
+      config.access_token_secret = YOUR_ACCESS_SECRET
     end
-  end
-
-  def tweet(message)
-    @client.update(message)
-  end
-
-  def follow(id)
-    @client.follow(id)
-  end
-
-  def fetch_user(id)
-    @client.user(id)
-  end
-
-  def search(message)
-    @client.search(message)
   end
 
   def search_amlovers
